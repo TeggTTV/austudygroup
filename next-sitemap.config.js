0e8733a02e95c56e1d274a0780eff00c315d4140
TeggTTV <doe034945@gmail.com>
2026-08-12 18:22:44 -0400

    chore: update siteUrl to default to Vercel production domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://austudygroup.edu.au',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-  },
+	siteUrl: process.env.SITE_URL || 'https://austudygroup.vercel.app',
+	generateRobotsTxt: true,
+	robotsTxtOptions: {
+		policies: [
+			{
+				userAgent: '*',
+				allow: '/',
+			},
+		],
+	},
 };
